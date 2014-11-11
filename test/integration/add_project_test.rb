2f336e84cdab05f08408bc2aeabb86797dcc675b
Marc <marc.bluemner@gmail.com>
2014-11-11 18:24:14 +0100

    project task

--- test/integration/add_project_test.rb
+++ test/integration/add_project_test.rb
@@ -1,15 +1,19 @@
-require 'test_helper'
+require "test_helper"
 
 class AddProjectTest < Capybara::Rails::TestCase
-
-  test "a user can add a new project and give it tasks" do
+  test "a user can add a a project and give it tasks" do
     visit new_project_path
-    fil_in "Name", with: "Project Runway"
+    fill_in "Name", with: "Project Runway"
     fill_in "Tasks", with: "Task 1:3\nTask2:5"
     click_on("Create Project")
     visit projects_path
-    assert_content("Project Runway")
-    assert_content("8")
+    @project = Project.find_by_name("Project Runway")
+    assert_selector("#project_#{@project.id} .name", text: "Project Runway")
+    assert_selector("#project_#{@project.id} .total-size", text: "8")
   end
+
 end
+
+
+
 
